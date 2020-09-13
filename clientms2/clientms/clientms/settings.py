eaeaca8b40a74897c054fa568da5334cb8f40588
Shweta Boddupalli <sboddupalli@unomaha.edu>
2020-09-12 22:34:01 -0500

    Updated Word doc
    Updated word doc and settings.py

--- clientms2/clientms/clientms/settings.py
+++ clientms2/clientms/clientms/settings.py
@@ -131,5 +131,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'boddupshweta@gmail.com'
-EMAIL_HOST_PASSWORD = 'Testlogin$100'
+EMAIL_HOST_PASSWORD = 'neenavvunaakuishtamraarey'
 EMAIL_USE_TLS = True
